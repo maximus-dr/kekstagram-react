d8b20c2450a813fa413d81b0f1390c5da6af7bb2
Max Ivanov <maximus-dr@mail.ru>
2021-02-04 00:09:41 +0300

    Added styles for PostLink

--- src/components/Main/AddPost/AddPost.js
+++ src/components/Main/AddPost/AddPost.js
@@ -3,22 +3,25 @@ import './AddPost.scss';
 
 export default function AddPost() {
   return (
-    <div className="add-post__wrapper">
-      <form className="add-post">
+    <section className="add-post">
+      <div className="add-post__wrapper">
 
-        <fieldset className="add-post__fieldset">
-          <input 
-            className="add-post__input visually-hidden" 
-            type="file" 
-            id="add-file"
-            required 
-          />
-          <label htmlFor="add-file" className="add-post__label add-post-control">
-            Загрузить
-          </label>
-        </fieldset>
-        
-      </form>
-    </div>
+        <form className="add-post__form">
+
+          <fieldset className="add-post__fieldset">
+            <input 
+              className="add-post__input visually-hidden" 
+              type="file" 
+              id="add-file"
+              required 
+            />
+            <label htmlFor="add-file" className="add-post__label add-post-control">
+              Загрузить
+            </label>
+          </fieldset>
+          
+        </form>
+      </div>
+    </section>
   )
 }
--- src/components/Main/PostList/PostLink.js
+++ src/components/Main/PostList/PostLink.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React from 'react';
+import './PostLink.scss';
 
 export default function PostLink({ post }) {
   return (
-    <div className="post__link"> 
+    <div className="post-link"> 
       <img className="post-link__img" src={post.url} alt="post" width="182" height="182" />
       <p className="post-link__stats">
         <span className="post-link__stat post-link__stat--comments">
