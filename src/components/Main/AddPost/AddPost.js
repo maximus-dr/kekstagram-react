8409329b7132341c7d8eaf6b43feef5b974893d1
Max Ivanov <maximus-dr@mail.ru>
2021-03-01 02:38:43 +0300

    Add Loader to AddPost component

--- src/components/Main/AddPost/AddPost.js
+++ src/components/Main/AddPost/AddPost.js
@@ -1,9 +1,10 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import Modal from '../../Modal/Modal';
 import './AddPost.scss';
 import NewPost from './NewPost/NewPost';
 import { connect, useDispatch } from 'react-redux';
 import { closeNewPostForm, uploadNewPostFile } from './../../../store/actions/newPostActions';
+import Loader from '../../Modal/Loader/Loader';
 
 
 function AddPost({ newPost}) {
@@ -11,13 +12,17 @@ function AddPost({ newPost}) {
   const dispatch = useDispatch();
   const openModal = Boolean(newPost.file);
   const inputRef = useRef();
+  
+  const [loading, setLoading] = useState(false);
 
   const onFileUpload = e => {
+    setLoading(true);
     const file = e.target.files[0];
     const reader = new FileReader();
     reader.readAsDataURL(file);
 
     reader.onload = () => {
+      setLoading(false);
       const imgSrc = reader.result;
       dispatch(uploadNewPostFile(file, imgSrc));
     };
@@ -30,33 +35,37 @@ function AddPost({ newPost}) {
 
 
   return (
-    <section className="add-post">
-      <div className="add-post__wrapper">
+    <>
+      <Loader isOpen={loading} />
 
-        <div className="add-post__field">
-          <div className="add-post__fieldset">
+      <section className="add-post">
+        <div className="add-post__wrapper">
 
-            <input 
-              className="add-post__input visually-hidden" 
-              type="file" 
-              id="add-file"
-              ref={inputRef}
-              onChange={onFileUpload}
-              required 
-            />
+          <div className="add-post__field">
+            <div className="add-post__fieldset">
 
-            <label htmlFor="add-file" className="add-post__label add-post-control">
-              Загрузить
-            </label>
+              <input 
+                className="add-post__input visually-hidden" 
+                type="file" 
+                id="add-file"
+                ref={inputRef}
+                onChange={onFileUpload}
+                required 
+              />
+
+              <label htmlFor="add-file" className="add-post__label add-post-control">
+                Загрузить
+              </label>
+            </div>
+
+            <Modal isOpen={openModal} close={onModalClose}>
+              <NewPost />
+            </Modal>
+            
           </div>
-
-          <Modal isOpen={openModal} close={onModalClose}>
-            <NewPost />
-          </Modal>
-          
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   )
 }
 
